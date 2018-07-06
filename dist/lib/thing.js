5401b000dec4e52015c03845080662dacf294e49
Philippe Coval <p.coval@samsung.com>
2018-07-06 17:43:10 +0200

    WIP: mod: dist/lib/thing.js (sandbox/rzr/devel/mastodon/iotjs/master)
    Change-Id: I7aa22f74fdd1d25f7410c73bff5e3556c75d83ff
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- dist/lib/thing.js
+++ dist/lib/thing.js
@@ -4,18 +4,15 @@
 
 'use strict';
 
+/**
+ * A Web Thing.
+ */
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Ajv = require('ajv');
-var ajv = new Ajv();
-
-/**
- * A Web Thing.
- */
 
 var Thing = function () {
   /**
