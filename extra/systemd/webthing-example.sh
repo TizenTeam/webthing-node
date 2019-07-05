866634ea838673c77ee58d2f6dd805af4fec4156
Philippe Coval <p.coval@samsung.com>
2019-07-05 16:13:24 +0200

    WIP: mod: extra/systemd/webthing-example.sh (sandbox/rzr/master)
    Change-Id: I8656990cec29231358f3bb233df28b284f9ab780
    Signed-off-by: Philippe Coval <p.coval@samsung.com>

--- extra/systemd/webthing-example.sh
+++ extra/systemd/webthing-example.sh
@@ -3,3 +3,14 @@ set -x
 set -e
 cd /usr/share/local/src/webthing-node
 iotjs example/simplest-thing.js 
+cd /usr/local/opt/webthing-node
+
+# TODO
+user=rzr
+
+HOME=/home/$user
+. ${HOME}/.bashrc
+node --version
+
+# iotjs example/simplest-thing.js 
+make -C example/platform artik1020
